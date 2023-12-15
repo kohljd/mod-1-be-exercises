1b25ced0c696cf032d62e255341c9a04396d86b8
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2023-12-15 11:32:11 -0600

    refactor to use Commentable module

--- lessons/modules/socials/lib/photo.rb
+++ lessons/modules/socials/lib/photo.rb
@@ -1,5 +1,7 @@
 # ./photo.rb
+require './lib/commentable'
 class Photo
+	include Commentable
 	attr_reader :image_source, :caption, :comments
 
 	def initialize(image_source, caption)
@@ -18,25 +20,5 @@ class Photo
 		end
 		display_string += "-" * 50
 		return display_string
-	end
-
-	def add_comment(comment)
-		comments[next_comment_id] = comment
-	end
-
-	def remove_comment(id)
-		comments.delete(id)
-	end
-
-	def next_comment_id
-		if total_comments == 0
-			total_comments + 1
-		else
-			comments.keys.last + 1
-		end
-	end
-
-	def total_comments
-		comments.count
 	end
 end
