9f43d6fcbc07f6248f92dcf33ad96e925895cc1c
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-01-01 17:15:45 -0600

    create charge method tracking amt charged and cc number

--- practice_independent_challenges/week4/boat_rental/lib/dock.rb
+++ practice_independent_challenges/week4/boat_rental/lib/dock.rb
@@ -12,4 +12,21 @@ class Dock
     def rent(boat, renter)
         @rental_log.store(boat, renter)
     end
+
+    def charge(boat)
+        charge_details = {}
+
+        @rental_log.find do |boat_element| 
+            boat_element == boat
+            boat_element.find do |boat_details|
+                if boat_details.hours_rented > @max_rental_time
+                    charge_details[:charge_amount] = boat_details.hourly_price * @max_rental_time
+                else
+                    charge_details[:charge_amount] = boat_details.hourly_price * boat_details.hours_rented
+                end
+            end
+            charge_details[:card_number] = boat_element.last.credit_card_number
+        end
+        charge_details
+    end
 end
