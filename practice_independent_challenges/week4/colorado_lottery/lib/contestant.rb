c57cc49dc4457b7bb5673f2503c10a768328b85b
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-01-02 19:50:46 -0600

    create contestant with their attributes

---
+++ practice_independent_challenges/week4/colorado_lottery/lib/contestant.rb
@@ -0,0 +1,19 @@
+class Contestant
+    attr_reader :first_name,
+                :last_name,
+                :age,
+                :state_of_residence,
+                :spending_money,
+                :full_name,
+                :game_interests
+    
+    def initialize(contestant_info)
+        @first_name = contestant_info[:first_name]
+        @last_name = contestant_info[:last_name]
+        @age = contestant_info[:age]
+        @state_of_residence = contestant_info[:state_of_residence]
+        @spending_money = contestant_info[:spending_money]
+        @full_name = contestant_info.values_at(:first_name, :last_name).join(" ")
+        @game_interests = []
+    end
+end
