8bf5a206fb49b9426cefdb9d45c7d43e543117d2
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-01-02 18:44:05 -0600

    refactor charge method and complete iteration 4

--- practice_independent_challenges/week4/boat_rental/lib/dock.rb
+++ practice_independent_challenges/week4/boat_rental/lib/dock.rb
@@ -1,32 +1,49 @@
 class Dock
     attr_reader :name,
                 :max_rental_time,
-                :rental_log
+                :rental_log,
+                :returned_boats,
+                :revenue
+
 
     def initialize(name, max_rental_time)
         @name = name
         @max_rental_time = max_rental_time
         @rental_log = {}
+        @returned_boats = []
+        @revenue = 0
     end
 
     def rent(boat, renter)
         @rental_log.store(boat, renter)
+        @returned_boats.delete(boat)
     end
 
     def charge(boat)
         charge_details = {}
 
-        @rental_log.find do |boat_element| 
-            boat_element == boat
-            boat_element.find do |boat_details|
-                if boat_details.hours_rented > @max_rental_time
-                    charge_details[:charge_amount] = boat_details.hourly_price * @max_rental_time
+        if @rental_log.include?(boat)
+                if boat.hours_rented > @max_rental_time
+                    charge_details[:charge_amount] = boat.hourly_price * @max_rental_time
                 else
-                    charge_details[:charge_amount] = boat_details.hourly_price * boat_details.hours_rented
+                    charge_details[:charge_amount] = boat.hourly_price * boat.hours_rented
                 end
-            end
-            charge_details[:card_number] = boat_element.last.credit_card_number
+            charge_details[:card_number] = @rental_log[boat].credit_card_number
         end
+        @revenue += charge_details[:charge_amount]
         charge_details
+    end
+
+    def return(boat)
+        @returned_boats << boat
+        charge(boat)
+    end
+
+    def log_hour
+        @rental_log.each do |boat, renter|
+            unless @returned_boats.include?(boat)
+                boat.add_hour
+            end
+        end
     end
 end
