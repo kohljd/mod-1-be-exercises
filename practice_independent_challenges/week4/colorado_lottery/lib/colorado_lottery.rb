09c6ee020cb3f4ecb759fe7657fc40fc2c243204
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-01-04 01:19:04 -0600

    Feat: register contestants and contestant list options added

--- practice_independent_challenges/week4/colorado_lottery/lib/colorado_lottery.rb
+++ practice_independent_challenges/week4/colorado_lottery/lib/colorado_lottery.rb
@@ -18,4 +18,27 @@ class ColoradoLottery
         return false unless interested_and_18?(contestant,game) == true && (contestant.out_of_state? == false || game.national_drawing? == true)
         true
     end
+
+    def register_contestant(contestant, game)
+        if can_register?(contestant, game)
+            @registered_contestants[game] ||= []
+            @registered_contestants[game] << contestant
+        end
+    end
+
+    def eligible_contestants(game)
+        @registered_contestants[game].select {|contestant| contestant.spending_money >= game.cost}
+    end
+
+    def current_contestants
+        @registered_contestants.each do |game, contestants|
+            @registered_contestants[game] = eligible_contestants(game)
+            @registered_contestants[game].each do |contestant|
+                contestant.charge(game.cost)
+                @current_contestants[game] ||= []
+                @current_contestants[game] << contestant.full_name
+            end
+        end
+        @current_contestants
+    end
 end
