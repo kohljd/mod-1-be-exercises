cc4daeb16ee77fdf15f5cd559e16be572532c63b
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-01-15 14:27:20 -0600

    Feat: add photo or artist

---
+++ practice_independent_challenges/week5/museo/lib/curator.rb
@@ -0,0 +1,17 @@
+class Curator
+    attr_reader :artists,
+                :photographs
+
+    def initialize
+        @artists = []
+        @photographs = []
+    end
+
+    def add_photograph(photo)
+        @photographs << photo
+    end
+
+    def add_artist(artist)
+        @artists << artist
+    end
+end
