8eaea2cdab3e2e1c2df4656e1605537f35e92f1b
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-01-15 14:37:24 -0600

    Test: list artists' works

--- practice_independent_challenges/week5/museo/spec/curator_spec.rb
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -61,4 +61,40 @@ RSpec.describe Curator do
 
         expect(@curator.find_artist_by_id("1")).to eq(artist_1)
     end
+
+    it 'can list artists & photos' do
+        photo_1 = Photograph.new({
+            id: "1",      
+            name: "Rue Mouffetard, Paris (Boy with Bottles)",      
+            artist_id: "1",      
+            year: "1954"      
+        })
+        photo_2 = Photograph.new({
+            id: "2",      
+            name: "Moonrise, Hernandez",      
+            artist_id: "2",      
+            year: "1941"      
+        })
+        artist_1 = Artist.new({
+            id: "1",      
+            name: "Henri Cartier-Bresson",      
+            born: "1908",      
+            died: "2004",      
+            country: "France"      
+        })
+        artist_2 = Artist.new({
+            id: "2",      
+            name: "Ansel Adams",      
+            born: "1902",      
+            died: "1984",      
+            country: "United States"      
+        })
+
+        @curator.add_photograph(photo_1)
+        @curator.add_photograph(photo_2)
+        @curator.add_artist(artist_1)
+        @curator.add_artist(artist_2)
+
+        expect(@curator.list_artists_works).to eq({artist_1 => photo_1, artist_2 => photo_2})
+    end
 end
