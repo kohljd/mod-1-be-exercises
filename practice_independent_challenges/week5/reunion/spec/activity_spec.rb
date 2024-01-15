46c1b40c20fa519ae49f40e38b569a4b99758b31
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-01-15 16:23:24 -0600

    Test: amount participant owes

--- practice_independent_challenges/week5/reunion/spec/activity_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/activity_spec.rb
@@ -35,4 +35,10 @@ RSpec.describe Activity do
         @activity.add_participant("Luther", 40)
         expect(@activity.split).to eq(30)
     end
+
+    it 'returns amount participants owe' do
+        @activity.add_participant("Maria", 20)
+        @activity.add_participant("Luther", 40)
+        expect(@activity.owed).to eq({"Maria" => 10, "Luther" => -10})
+    end
 end
