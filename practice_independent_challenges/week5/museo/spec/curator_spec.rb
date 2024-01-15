3a8a22aaebbe80b68ee66e5f17c52ca6d12ec052
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-01-15 14:44:06 -0600

    Test: list photos from a country

--- practice_independent_challenges/week5/museo/spec/curator_spec.rb
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -133,4 +133,40 @@ RSpec.describe Curator do
 
         expect(@curator.artists_with_multiple_works).to eq(artist_1.name)
     end
+
+    it 'can list photos by artist country' do
+        photo_1 = Photograph.new({
+            id: "1",      
+            name: "Rue Mouffetard, Paris (Boy with Bottles)",      
+            artist_id: "1",      
+            year: "1954"      
+        })
+        photo_2 = Photograph.new({
+            id: "1",      
+            name: "Moonrise, Hernandez",      
+            artist_id: "2",      
+            year: "1941"      
+        })
+        artist_1 = Artist.new({
+            id: "1",      
+            name: "Henri Cartier-Bresson",      
+            born: "1908",      
+            died: "2004",      
+            country: "France"      
+        })
+        artist_2 = Artist.new({
+            id: "2",      
+            name: "Ansel Adams",      
+            born: "1902",      
+            died: "1984",      
+            country: "United States"      
+        })
+
+        @curator.add_photograph(photo_1)
+        @curator.add_photograph(photo_2)
+        @curator.add_artist(artist_1)
+        @curator.add_artist(artist_2)
+
+        expect(@curator.photos_from_country("France")).to eq([photo_1])
+    end
 end
