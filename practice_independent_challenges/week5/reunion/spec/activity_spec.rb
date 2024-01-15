73bbf79c7ba59d421c485e1349a10b4a398e40ab
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-01-15 16:04:27 -0600

    Test: activity

---
+++ practice_independent_challenges/week5/reunion/spec/activity_spec.rb
@@ -0,0 +1,32 @@
+require './spec/spec_helper'
+
+RSpec.describe Activity do
+    before(:each) do
+         @activity = Activity.new("Brunch")
+    end
+
+    it 'exists' do
+        expect(@activity).to be_an_instance_of(Activity)
+    end
+
+    it 'has a name' do
+        expect(@activity.name).to eq("Brunch")
+    end
+
+    it 'starts with 0 participants' do
+        expect(@activity.participants).to eq({})
+    end
+
+    it 'can add participants' do
+        @activity.add_participant("Maria", 20)
+        expect(@activity.participants).to eq({"Maria" => 20})
+    end
+
+    it 'returns total cost' do
+        @activity.add_participant("Maria", 20)
+        expect(@activity.total_cost).to eq(20)
+
+        @activity.add_participant("Luther", 40)
+        expect(@activity.total_cost).to eq(60)
+    end
+end
